05b0087fe6b1aa993289bd6132c2cc3613ac064e
Nils Dralle <nils.91@googlemail.com>
2018-05-15 10:34:38 +0200

    naive bayes pred

--- nb.py
+++ nb.py
@@ -6,3 +6,7 @@ classifier = GaussianNB()
 classifier.fit(iris.data, iris.target)
 score = metrics.accuracy_score(iris.target, classifier.predict(iris.data))
 print("Accuracy: %f" % score)
+
+newfeatures=[4.9 3.1 1.5 0.2],[5 3 1.5 0.2],[6 2.5 1.5 0.2],[4.2 1.3 1.7 0],[5.15 2.9 3.5 1.2]
+for f in newfeatures
+	print("Prediction: %i" % classifier.predict(f));
