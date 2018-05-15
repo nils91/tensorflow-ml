64f17f40f0530546a1ee874fc4a2a4e6dc45c2da
Nils Dralle <nils.91@googlemail.com>
2018-05-15 10:43:54 +0200

    naive bayes fix syn

--- nb.py
+++ nb.py
@@ -8,4 +8,6 @@ score = metrics.accuracy_score(iris.target, classifier.predict(iris.data))
 print("Accuracy: %f" % score)
 
 newfeatures=[[4.9,3.1,1.5,0.2],[5,3,1.5,0.2],[6,2.5,1.5,0.2],[4.2,1.3,1.7,0],[5.15,2.9,3.5,1.2]]
-print("Prediction: %i" % classifier.predict(newfeatures));
+predictions=classifier.predict(newfeatures)
+for p in predictions:
+	print("Prediction: %i" % p);
