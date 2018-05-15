e85f51cff5e070ddbf5b680f844d465c02e0245e
Nils Dralle <nils.91@googlemail.com>
2018-05-15 08:46:10 +0200

    parse training dataset

--- script.py
+++ script.py
@@ -6,6 +6,15 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import tensorflow.contrib.eager as tfe
 
+def parse_csv(line):
+  example_defaults = [[0.], [0.], [0.], [0.], [0]]  # sets field types
+  parsed_line = tf.decode_csv(line, example_defaults)
+  # First 4 fields are features, combine into single tensor
+  features = tf.reshape(parsed_line[:-1], shape=(4,))
+  # Last field is the label
+  label = tf.reshape(parsed_line[-1], shape=())
+  return features, label
+
 tf.enable_eager_execution()
 
 print("TensorFlow version: {}".format(tf.VERSION))
@@ -17,3 +26,14 @@ train_dataset_fp = tf.keras.utils.get_file(fname=os.path.basename(train_dataset_
                                            origin=train_dataset_url)
 
 print("Local copy of the dataset file: {}".format(train_dataset_fp))
+
+train_dataset = tf.data.TextLineDataset(train_dataset_fp)
+train_dataset = train_dataset.skip(1)             # skip the first header row
+train_dataset = train_dataset.map(parse_csv)      # parse each row
+train_dataset = train_dataset.shuffle(buffer_size=1000)  # randomize
+train_dataset = train_dataset.batch(32)
+
+# View a single example entry from a batch
+features, label = iter(train_dataset).next()
+print("example features:", features[0])
+print("example label:", label[0])
