4e99beca52913922f27fcc3da00969b8fdda1be1
Nils Dralle <nils.91@googlemail.com>
2018-05-15 10:17:51 +0200

    naive bayes

---
+++ nb.py
@@ -0,0 +1,8 @@
+from sklearn.naive_bayes import GaussianNB
+from sklearn import datasets, metrics
+
+iris = datasets.load_iris()
+classifier = GaussianNB()
+classifier.fit(iris.data, iris.target)
+score = metrics.accuracy_score(iris.target, classifier.predict(iris.data))
+print("Accuracy: %f" % score)
